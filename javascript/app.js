6074e1e36126fee5901574b0314bf9b001a7299c
Shunji Sugaya <sugayasc33@gmail.com>
2025-06-15 18:27:48 +0900

    replaced aos with gsap

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,21 @@
-AOS.init();
-let samt = 0;
-window.addEventListener('scroll', function() {
-samt <= 10 ? samt++ : AOS.refresh();
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray('.card').forEach(card => {
+  gsap.fromTo(card,
+    {
+      opacity: 0,
+      y: 50
+    },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 0.8,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: card,
+        start: "top 80%",
+        toggleActions: "play reverse play reverse"
+      }
+    }
+  );
 });
